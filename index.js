5f8342326219c1c7035e18680e6e15f219f214d5
Gavin Jin <gavinjin62@gmail.com>
2019-04-14 23:27:41 -0700

    initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const fs = require("fs");
+const http = require("http");
+const url = require("url");
+
+const json = fs.readFileSync(`${__dirname}/data/data.json`, "utf-8");
+const laptopData = JSON.parse(json);
+
+const server = http.createServer((req, res) => {
+  const pathName = url.parse(req.url, true).pathname;
+  const id = url.parse(req.url, true).query.id;
+  console.log(id);
+
+  if (pathName === "/products" || pathName === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end("This is the products page");
+  } else if (pathName === "/laptop") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.end(`This is the laptop page for ${id}`);
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.end("URL was not found on the server");
+  }
+});
+
+server.listen(1337, "127.0.0.1", () => {
+  console.log("listening to requests");
+});
