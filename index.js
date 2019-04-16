2d4de26c3931bcbda04795a3b605b7dccb08ab20
Gavin Jin <gavinjin62@gmail.com>
2019-04-15 19:50:29 -0700

    append json content to html template

--- index.js
+++ index.js
@@ -8,14 +8,28 @@ const laptopData = JSON.parse(json);
 const server = http.createServer((req, res) => {
   const pathName = url.parse(req.url, true).pathname;
   const id = url.parse(req.url, true).query.id;
-  console.log(id);
 
   if (pathName === "/products" || pathName === "/") {
     res.writeHead(200, { "content-type": "text/html" });
     res.end("This is the products page");
   } else if (pathName === "/laptop" && id < laptopData.length) {
     res.writeHead(200, { "content-type": "text/html" });
-    res.end(`This is the laptop page for ${id}`);
+    fs.readFile(
+      `${__dirname}/templates/template-laptop.html`,
+      "utf-8",
+      (err, data) => {
+        const laptop = laptopData[id];
+        let output = data.replace(/{%PRODUCTNAME%}/g, laptop.productName);
+        output = output.replace(/{%IMAGE%}/g, laptop.image);
+        output = output.replace(/{%PRICE%}/g, laptop.price);
+        output = output.replace(/{%SCREEN%}/g, laptop.screen);
+        output = output.replace(/{%CPU%}/g, laptop.cpu);
+        output = output.replace(/{%STORAGE%}/g, laptop.storage);
+        output = output.replace(/{%RAM%}/g, laptop.ram);
+        output = output.replace(/{%DESCRIPTION%}/g, laptop.description);
+        res.end(output);
+      }
+    );
   } else {
     res.writeHead(404, { "content-type": "text/html" });
     res.end("URL was not found on the server");
